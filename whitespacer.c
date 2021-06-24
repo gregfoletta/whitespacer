2e58d2c27b019ce526bd209af4fd6411bc38fec9
Greg Foletta <greg@foletta.org>
2021-06-25 05:49:58 +1000

    Moved to lookup table with differing whitespace

--- whitespacer.c
+++ whitespacer.c
@@ -3,39 +3,52 @@
 #include <unistd.h>
 #include <string.h>
 
+#define INPUT_OUTPUT_RATIO 4
 #define READ_BUFFER 10485760 
-#define OUTPUT_BUFFER READ_BUFFER * 4
+#define OUTPUT_BUFFER READ_BUFFER * INPUT_OUTPUT_RATIO
 
 #define STDIN 0
 #define STDOUT 1
 
-//Quaternary translation - these need to be contiguous characters.
-char ws_lookup[] = {'\b', '\t', '\n', '\v'};
+//Lookup table for each of the 2-bit values
+char encode_lookup[] = {
+    '\t', '\n', '\r', ' '
+};
+
+unsigned char decode_lookup[] = {
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 0, 1, 255, 255, 2, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    3
+};
+
+char ws_lookup[] = {'\t', '\n', '\v', '\f'};
 
 //Encode each 2 bits of the byte into whitespace
-int ws_encode(unsigned char *bytes_in, unsigned char *ws_out, ssize_t bytes) {
+int ws_encode(const unsigned char *bytes_in, unsigned char *ws_out, const ssize_t bytes) {
     int x;
 
     for (x = 0; x < bytes; x++) {
-        *ws_out++ = (bytes_in[x] & 0x03) + ws_lookup[0];
-        *ws_out++ = (bytes_in[x] >> 2 & 0x03) + ws_lookup[0];
-        *ws_out++ = (bytes_in[x] >> 4 & 0x03) + ws_lookup[0];
-        *ws_out++ = (bytes_in[x] >> 6 & 0x03) + ws_lookup[0];
+        *ws_out++ = encode_lookup[ bytes_in[x] & 0x03 ];
+        *ws_out++ = encode_lookup[ (bytes_in[x] >> 2) & 0x03 ];
+        *ws_out++ = encode_lookup[ (bytes_in[x] >> 4) & 0x03 ];
+        *ws_out++ = encode_lookup[ (bytes_in[x] >> 6) & 0x03 ];
     }
 
     return x * 4;
 }
 
 //Decode each group of four bytes back into a single byte.
-int ws_decode(unsigned char *bytes_in, unsigned char *bytes_out, ssize_t bytes_read) {
+int ws_decode(const unsigned char *bytes_in, unsigned char *bytes_out, const ssize_t bytes_read) {
     int x;
 
     for (x = 0; x < bytes_read; x += 4) {
         *bytes_out++ = 
-            bytes_in[x] - ws_lookup[0] |
-            (bytes_in[x + 1] - ws_lookup[0]) << 2 |
-            (bytes_in[x + 2] - ws_lookup[0]) << 4 |
-            (bytes_in[x + 3] - ws_lookup[0]) << 6;
+            decode_lookup[ bytes_in[x] ] |
+            decode_lookup[ bytes_in[x + 1] ] << 2 |
+            decode_lookup[ bytes_in[x + 2] ] << 4 |
+            decode_lookup[ bytes_in[x + 3] ] << 6;
     }
     
     return x / 4;
@@ -55,18 +68,26 @@ int is_decoder(int argc, char **argv) {
 int main(int argc, char **argv) {
     ssize_t bytes_read = 0, total_bytes_read = 0, num_bytes_write = 0;
     unsigned char *bytes_in, *bytes_out;
+    int ret;
 
     int decoding = is_decoder(argc, argv);
 
     bytes_in = malloc(READ_BUFFER * sizeof(*bytes_in));
-    bytes_out = malloc(4 * READ_BUFFER * sizeof(*bytes_in));
+    bytes_out = malloc(OUTPUT_BUFFER * sizeof(*bytes_in));
+
+    if (!bytes_in || !bytes_out) {
+        fprintf(stderr, "- Could not allocate memory for input/output\n");
+        ret = -1;
+        goto out;
+    }
 
     unsigned char *buffer_pos = bytes_in;
 
     while ( (bytes_read = read(STDIN, buffer_pos, READ_BUFFER - total_bytes_read)) ) {
         if (bytes_read < 0) {
             fprintf(stderr, "- read() error\n");
-            return -1;
+            ret = -1;
+            goto out;
         }
 
         total_bytes_read += bytes_read;
@@ -88,7 +109,7 @@ int main(int argc, char **argv) {
         //Write out to stdout
         if ( write(STDOUT, bytes_out, num_bytes_write) < 0 ) {
             fprintf(stderr, "- write() error\n");
-            return -1;
+            ret = -1;
         }
 
         //Reset buffer position and total bytes read.
@@ -100,10 +121,16 @@ int main(int argc, char **argv) {
     //a multiple of 4 bytes and our decoding failed.
     if (total_bytes_read != 0) {
         fprintf(stderr, "- Error: decoding and input not a multiple of 4 bytes\n");
-        return -1;
+        ret = -1;
+        goto out;
     }
 
-    return 0;
+    ret = 0;
+
+out:
+    free(bytes_in);
+    free(bytes_out);
+    return ret;
 }
 
 
