921b0c45499e696d80268468a82ce9508c63b4e7
Greg Foletta <greg@foletta.org>
2021-09-23 16:31:49 +1000

    Test other algorithms

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,9 @@ make
 dd if=/dev/urandom of=urandom bs=1MB count=32
 
 cat urandom | ./ws | ./ws -d > urandom.trans
+cat urandom | ./ws -a poly | ./ws -d -a poly > urandom.poly.trans
+cat urandom | ./ws -a switch | ./ws -d -a switch > urandom.switch.trans
+cat urandom | ./ws -a switch | ./ws -d -a poly > urandom.switch.poly.trans
 md5sum urandom*
 
 make clean
